a0e5b8a999410b6156baf2260dfb385b3f29b68c
timSea <1766853670@qq.com>
2026-04-10 13:40:25 +0800

    flash attention

--- cutitle/flash_attn.py
+++ cutitle/flash_attn.py
@@ -1,24 +1,54 @@
 import torch
 import cuda.tile as ct
 
+
+
 @ct.kernel
-def ct_sum(x: ct.Array, y: ct.Array, tile_size: ct.Constant):
-    # x, y: 1d array with any length
-    block_id = ct.bid(0)
-    tile_x = ct.load(x, index=(block_id, ), shape=(tile_size, ))
-    tile_x = ct.sum(tile_x.astype(ct.float32))
-    ct.atomic_add(y, (0, ), tile_x.astype(y.dtype))
+def flash_attn(Q:ct.Array, K:ct.Array, V:ct.Array, O:ct.Array,
+                tileS:ct.Constant, tileD:ct.Constant):
+                 
+    # block mapping on O[B(block_z 1:1), S(block_x 1:tilees)， H(block_y 1:1)， D]
+    block_x, block_y,block_z = ct.bid(0),  ct.bid(1) , ct.bid(2)
+    tile_Q = ct.load(Q, (block_z, block_x, block_y, 0), (1, tileS, 1, tileD)).reshape((tileS, tileD))
 
-x = torch.randn(size=[1024 * 1024], device="cuda", dtype=torch.float16)
-y = torch.zeros(size=(1, ), device="cuda", dtype=torch.float16)
+    accumlator = ct.full((tileS,tileD), 0.0, dtype=ct.float32)
+    for seq_iter in range(ct.cdiv(K.shape[1], tileS)):
+        tile_K = ct.load(K, (block_z, block_x, block_y, 0), (1, tileS, 1, tileD)).reshape((tileS, tileD))
+        tile_K = ct.transpose(tile_K, 0, 1)
 
-tile_size: int = 256
-num_elements = x.numel()
-num_blocks = (num_elements // tile_size)
+        tile_QK = ct.matmul(tile_Q, tile_K) # [tileS, tileS]
+        tile_V = ct.load(V, (block_z, block_x, block_y, 0), (1, tileS, 1, tileD)).reshape((tileS, tileD))
 
-ct.launch(
-    torch.cuda.current_stream(), (num_blocks, ), 
-    ct_sum, (x, y, tile_size)
+        accumlator = ct.mma(tile_QK, tile_V, accumlator)
+        
+    accumlator = accumlator.reshape((1,tileS,1, tileD))
+    ct.store(O,(block_z,block_x,block_y,0), accumlator)
+# Q =  [batch size, sequence size, num of head, head dim]
+
+B, S, H ,D = 1, 128, 8, 64
+Q = torch.randn(size=[B, S, H, D], device = "cuda", dtype = torch.float32)
+K = torch.randn(size=[B, S, H, D], device = "cuda", dtype = torch.float32)
+V = torch.randn(size=[B, S, H, D], device = "cuda", dtype = torch.float32)
+O = torch.empty_like(Q)
+
+_Q = Q.permute(0, 2, 1, 3)
+_K = K.permute(0, 2, 1, 3)
+_V = V.permute(0, 2, 1, 3)
+
+
+real = torch.softmax(_Q @ _K.transpose(-1,-2), dim=1)@ _V
+real = real.permute(0, 2, 1, 3)
+
+ref = _Q @ _V.transpose(-1,-2) @ _V
+ref = ref.permute(0, 2, 1, 3)
+
+ct.launch(torch.cuda.current_stream(),
+        (ct.cdiv(S,32),H,B),
+        flash_attn,
+        (Q,K,V,O,32,D)
 )
 
-print(y)
+print(ref - O)       
+        
+
+
